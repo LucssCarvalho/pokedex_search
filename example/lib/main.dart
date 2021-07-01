f6063268c538ad19b1233aff58e82fdc81ab2e9d
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-06-30 22:22:14 -0300

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -48,9 +48,15 @@ class _MyHomePageState extends State<MyHomePage> {
           child: FutureBuilder<PokedexSearch>(
               future: PokedexSearch.getListPokemons(),
               builder: (context, snapshot) {
-                return Text(snapshot.data == null
+                return snapshot.data == null
                     ? ''
-                    : snapshot.data.pokemon[45].name);
+                    : ListView.builder(
+                        itemCount: snapshot.data.pokemon.length,
+                        itemBuilder: (_, i) {
+                          return ListTile(
+                            title: Text('${snapshot.data.pokemon[i].name}'),
+                          );
+                        });
               })),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -59,71 +65,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
-
-Widget pokemonTile(Pokemon pokemon) {
-  return GestureDetector(
-    onTap: () {
-      List<Pokemon> nextEvolution = [];
-      List<Pokemon> prevEvolution = [];
-
-      if (pokemon.nextEvolution != null) {
-        pokemon.nextEvolution.forEach((pokes) {
-          nextEvolution.add(firstGen.pokemon
-              .firstWhere((element) => pokes.name == element.name));
-        });
-      }
-
-      if (pokemon.prevEvolution != null) {
-        pokemon.prevEvolution.forEach((pokes) {
-          prevEvolution.add(firstGen.pokemon
-              .firstWhere((element) => pokes.name == element.name));
-        });
-      }
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => PokemonDetail(
-              pokemon, nextEvolution, prevEvolution, firstGen.pokemon),
-        ),
-      );
-    },
-    child: Container(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  pokemon.name.toUpperCase(),
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "#${pokemon.num.toUpperCase()}",
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              getTypes(pokemon),
-              getPokemonImage(pokemon),
-            ],
-          )
-        ],
-      ),
-      decoration: BoxDecoration(
-          color: ConstColors.getColorType(type: pokemon.type[0]),
-          borderRadius: BorderRadius.circular(15)),
-    ),
-  );
 }
