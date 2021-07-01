0112a047b262285faeae356c5302bc963f8e409b
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-06-30 22:52:37 -0300

    add unity test

--- test/pokedex_search_test.dart
+++ test/pokedex_search_test.dart
@@ -2,4 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pokedex_search/pokedex_search.dart';
 
-void main() {}
+Future<void> main() async {
+  final pokes = await PokedexSearch.getListPokemons();
+
+  test('a list of 151 Pokemons', () {
+    expect(pokes.pokemon.length, 151);
+  });
+}
