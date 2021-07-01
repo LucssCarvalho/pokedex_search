9f2c9165da150f1e8dfba97dd6186c4d6f789db3
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-07-01 01:44:08 -0300

    update example

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,5 +1,6 @@
 library pokedex_search;
 
+import 'package:flutter/material.dart';
 import 'package:pokedex_search/src/pokedex_repository.dart';
 
 class PokedexSearch {
@@ -149,5 +150,69 @@ class PrevEvolution {
     data['num'] = this.num;
     data['name'] = this.name;
     return data;
+  }
+}
+
+class ConstColors {
+  static Color getColorType(String type) {
+    switch (type) {
+      case 'Normal':
+        return Colors.brown[400];
+        break;
+      case 'Fire':
+        return Colors.red[400];
+        break;
+      case 'Water':
+        return Colors.blue[300];
+        break;
+      case 'Grass':
+        return Colors.green[400];
+        break;
+      case 'Electric':
+        return Colors.amber[300];
+        break;
+      case 'Ice':
+        return Colors.cyanAccent[400];
+        break;
+      case 'Fighting':
+        return Colors.orange;
+        break;
+      case 'Poison':
+        return Colors.purple[300];
+        break;
+      case 'Ground':
+        return Colors.orange[300];
+        break;
+      case 'Flying':
+        return Colors.indigo[200];
+        break;
+      case 'Psychic':
+        return Colors.pink[400];
+        break;
+      case 'Bug':
+        return Colors.lightGreen[500];
+        break;
+      case 'Rock':
+        return Colors.grey;
+        break;
+      case 'Ghost':
+        return Colors.indigo[400];
+        break;
+      case 'Dark':
+        return Colors.brown;
+        break;
+      case 'Dragon':
+        return Colors.indigo[800];
+        break;
+      case 'Steel':
+        return Colors.blueGrey;
+        break;
+      case 'Fairy':
+        return Colors.pinkAccent[100];
+        break;
+      default:
+        return Colors.grey;
+        break;
+    }
   }
 }
--- test/pokedex_search_test.dart
+++ test/pokedex_search_test.dart
@@ -1,11 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pokedex_search/pokedex_search.dart';
 
 Future<void> main() async {
-  final pokes = await PokedexSearch.getListPokemons();
+  final pokedexSearch = await PokedexSearch.getListPokemons();
 
   test('a list of 151 Pokemons', () {
-    expect(pokes.pokemon.length, 151);
+    expect(pokedexSearch.pokemon.length, 151);
   });
 }
